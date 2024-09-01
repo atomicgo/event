3c3937bf6cc96d0762be3489ce21632c0f740e83
MarvinJWendt <git@marvinjwendt.com>
2024-09-02 01:45:43 +0200

    feat: add event package

--- event.go
+++ event.go
@@ -1,1 +1,53 @@
 package event
+
+import "sync"
+
+// Event represents an event system that can handle multiple listeners.
+type Event[T any] struct {
+	listeners []chan T
+	mu        sync.Mutex
+}
+
+func New[T any]() *Event[T] {
+	return &Event[T]{
+		listeners: []chan T{},
+	}
+}
+
+// Trigger triggers the event and notifies all listeners.
+func (e *Event[T]) Trigger(value T) {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+
+	for _, listener := range e.listeners {
+		go func(l chan T) {
+			l <- value
+		}(listener)
+	}
+}
+
+// Listen gets called when the event is triggered.
+func (e *Event[T]) Listen(f func(T)) {
+	ch := make(chan T)
+
+	e.mu.Lock()
+	e.listeners = append(e.listeners, ch)
+	e.mu.Unlock()
+
+	go func() {
+		for v := range ch {
+			f(v)
+		}
+	}()
+}
+
+func (e *Event[T]) Close() {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+
+	for _, listener := range e.listeners {
+		close(listener)
+	}
+
+	e.listeners = nil
+}
--- examples_test.go
+++ examples_test.go
@@ -2,9 +2,39 @@ package event_test
 
 import (
 	"fmt"
+	"time"
+
+	"atomicgo.dev/event"
 )
 
+type Player struct {
+	Name string
+}
+
+var PlayerJoinEvent = event.New[Player]()
+
 func Example_demo() {
-	fmt.Println("Hello, World!")
-	// Output: Hello, World!
+	// Listen to the event as many times as you want
+	PlayerJoinEvent.Listen(func(p Player) {
+		fmt.Printf("Player %q joined the game\n", p.Name)
+	})
+
+	PlayerJoinEvent.Listen(func(p Player) {
+		// Do something else
+	})
+
+	// ...
+
+	// Trigger the event somewhere - can be in a different function or package
+	PlayerJoinEvent.Trigger(Player{Name: "Marvin"})
+	PlayerJoinEvent.Trigger(Player{Name: "Bob"})
+	PlayerJoinEvent.Trigger(Player{Name: "Alice"})
+
+	// Keep the program alive
+	time.Sleep(time.Second)
+
+	// Output:
+	// Player "Marvin" joined the game
+	// Player "Bob" joined the game
+	// Player "Alice" joined the game
 }
