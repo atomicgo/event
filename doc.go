eaf9942acc86baf235dfba229e5b7e74dd55b630
Marvin Wendt <git@marvinjwendt.com>
2024-09-12 22:40:53 +0200

    feat!: performance improvements and better thread safety (#1)
    * feat!: performance improvements and better thread safety
    
    * ci: exclude errcheck in test files

--- doc.go
+++ doc.go
@@ -1,4 +1,5 @@
-/*
-Package event provides a generic event system for Go.
-*/
+// Package event provides a generic and thread-safe event system for Go.
+// It allows multiple listeners to subscribe to events carrying data of any type.
+// Listeners can be added and notified when events are triggered, and the event
+// can be closed to prevent further operations.
 package event
